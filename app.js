6e91576c43402014211dad1a32a80c1bc8f72865
Ethan Harsh <ethan@ethanharsh.com>
2021-08-08 02:04:35 +0000

    handlebars init

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bars = require('express-handlebars');
 const catchAsync = require('./utils/catchAsync');
 
 const app = express();
+
+app.engine('handlebars', bars());
+app.set('view engine', 'handlebars')
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -9,6 +13,10 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
+});
+
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
 
